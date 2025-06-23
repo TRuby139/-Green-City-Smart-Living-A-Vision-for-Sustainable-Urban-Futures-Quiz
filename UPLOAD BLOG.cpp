4c628dbf8ef7d01827fdbb7a58fc8813fc76f289
tbk0809 <boonkaitan0809@gmail.com>
2025-06-23 15:33:46 +0800

    Update UPLOAD BLOG.cpp

--- UPLOAD BLOG.cpp
+++ UPLOAD BLOG.cpp
@@ -3,16 +3,21 @@
 using namespace std;
 string title, content;
 int options;
+
 int main() {
     while (true) {
         cout << "what u want" << endl;
-        cout << "upload a blog" << endl;
-        cout << "view blogs" << endl;
-        cout << "ur option?";
+        cout << "1)Upload a blog" << endl;
+        cout << "2)View blogs" << endl;
+        cout << "3)Exit"<<endl;
+        cout << "3)Ur option?";
         cin >> options;
+        cin.ignore();
         if (options == 1) {
             cout << "enter blog title";
+            cin.ignore();
             getline(cin, title);
+            
             cout << "\nenter blog content";
             getline(cin, content);
         }
@@ -21,8 +26,12 @@ int main() {
             cout << "Title of blog" << title << endl;
             cout << "Content      :" << content << endl;
         }
+        else if (options == 3) {
+            cout << "Exiting program..." << endl;
+            break;
+        }
         else {
-            cout << "invalid dude";
+            cout << "Invalid option, please select 1–3." << endl;
         }
 
     }
